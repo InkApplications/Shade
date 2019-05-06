f2aafa40b63b2620b84115a1e818e320e0206ccc
Renee Vandervelde <Renee@ReneeVandervelde.com>
2019-05-06 09:06:32 -0700

    Add API for interfacing with Groups

---
+++ groups/src/main/kotlin/inkapplications/shade/groups/GroupsModule.kt
@@ -0,0 +1,35 @@
+package inkapplications.shade.groups
+
+import com.squareup.moshi.Moshi
+import inkapplications.shade.auth.TokenStorage
+import inkapplications.shade.config.ShadeConfig
+import inkapplications.shade.serialization.adapter.ShadeDeferredCallAdapterFactory
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+/**
+ * Constructs Groups services.
+ */
+class GroupsModule {
+    /**
+     * Create new instance of the Groups services.
+     *
+     * @param client Client to create hue requests with
+     * @param config App-wide configuration for Shade, used to set up connections.
+     * @param tokenStorage A place to read/write the auth token used for requests.
+     */
+    fun createGroups(client: OkHttpClient, config: ShadeConfig, tokenStorage: TokenStorage): ShadeGroups {
+        val moshi = Moshi.Builder().build()
+
+        val retrofit = Retrofit.Builder()
+            .client(client)
+            .baseUrl(config.baseUrl)
+            .addCallAdapterFactory(ShadeDeferredCallAdapterFactory)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+        val api = retrofit.create(HueGroupsApi::class.java)
+
+        return ApiGroups(api, tokenStorage)
+    }
+}
---
+++ groups/src/main/kotlin/inkapplications/shade/groups/ShadeGroups.kt
@@ -0,0 +1,27 @@
+package inkapplications.shade.groups
+
+import inkapplications.shade.auth.TokenStorage
+import inkapplications.shade.auth.UnauthorizedException
+import kotlinx.coroutines.Deferred
+
+/**
+ * Access for Hue's various Groups.
+ */
+interface ShadeGroups {
+    /**
+     * Gets a list of all groups that have been added to the bridge.
+     */
+    suspend fun getGroups(): Deferred<Map<String, Group>>
+}
+
+/**
+ * API implementation of Shade's group functionality.
+ */
+internal class ApiGroups(
+    private val groupsApi: HueGroupsApi,
+    private val storage: TokenStorage
+): ShadeGroups {
+    private suspend fun getToken(): String = storage.getToken() ?: throw UnauthorizedException()
+
+    override suspend fun getGroups(): Deferred<Map<String, Group>> = groupsApi.getAll(getToken())
+}
