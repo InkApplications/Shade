766ad1a9d5d14e5d1525d4747246f50d0afc5e24
Renee Vandervelde <Renee@ReneeVandervelde.com>
2019-11-06 19:31:55 -0600

    Add Group list command to CLI

---
+++ cli/src/main/kotlin/inkapplications/shade/cli/groups/GroupsModule.kt
@@ -0,0 +1,13 @@
+package inkapplications.shade.cli.groups
+
+import com.github.ajalt.clikt.core.CliktCommand
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoSet
+
+@Module
+abstract class GroupsModule {
+    @Binds
+    @IntoSet
+    abstract fun list(command: GroupsList): CliktCommand
+}
