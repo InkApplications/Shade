402e5668ddd38bfbfc47a2104dcb042216eab9c4
Renee Vandervelde <Renee@ReneeVandervelde.com>
2020-07-19 08:45:01 -0500

    Update External dependencies

--- buildSrc/src/main/kotlin/SharedDependencies.kt
+++ buildSrc/src/main/kotlin/SharedDependencies.kt
@@ -2,27 +2,27 @@ import org.gradle.kotlin.dsl.DependencyHandlerScope
 
 fun DependencyHandlerScope.coroutines(
     module: String = "core",
-    version: String = "1.3.2"
+    version: String = "1.3.8"
 ) = "org.jetbrains.kotlinx:kotlinx-coroutines-$module:$version"
 
 fun DependencyHandlerScope.retrofit(
     module: String = "retrofit",
-    version: String = "2.6.1"
+    version: String = "2.9.0"
 ) = "com.squareup.retrofit2:$module:$version"
 
 fun DependencyHandlerScope.moshi(
     module: String = "moshi",
-    version: String = "1.8.0"
+    version: String = "1.9.3"
 ) = "com.squareup.moshi:$module:$version"
 
 fun DependencyHandlerScope.okHttp(
     module: String = "okhttp",
-    version: String = "4.2.2"
+    version: String = "4.8.0"
 ) = "com.squareup.okhttp3:$module:$version"
 
 fun DependencyHandlerScope.threeTen(
     module: String = "threetenbp",
-    version: String = "1.3.6",
+    version: String = "1.4.4",
     includeTimezoneDb: Boolean = false
 ) = "org.threeten:$module:$version" + (":no-tzdb".takeIf { !includeTimezoneDb }.orEmpty())
 
