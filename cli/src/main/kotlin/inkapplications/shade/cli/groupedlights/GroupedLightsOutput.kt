4566d93b5f8d9042d459f7289fdd2a5f5f73e335
Renee Vandervelde <Renee@ReneeVandervelde.com>
2022-12-17 10:52:00 -0600

    Update Grouped light parameters and docs

--- cli/src/main/kotlin/inkapplications/shade/cli/groupedlights/GroupedLightsOutput.kt
+++ cli/src/main/kotlin/inkapplications/shade/cli/groupedlights/GroupedLightsOutput.kt
@@ -5,8 +5,12 @@ import inkapplications.shade.groupedlights.structures.GroupedLight
 
 fun echoGroup(group: GroupedLight) {
     TermUi.echo("${group.id.value}:")
+    TermUi.echo("    Owner: ${group.owner}")
     group.powerInfo?.run {
         TermUi.echo("    On: $on")
+    }
+    group.dimmingInfo?.run {
+        TermUi.echo("    Dimming: $brightness")
     }
     group.alertInfo?.run {
         TermUi.echo("    Alerts: ${actionValues.joinToString()}")
--- grouped-lights/src/commonMain/kotlin/inkapplications/shade/groupedlights/structures/GroupedLight.kt
+++ grouped-lights/src/commonMain/kotlin/inkapplications/shade/groupedlights/structures/GroupedLight.kt
@@ -1,8 +1,10 @@
 package inkapplications.shade.groupedlights.structures
 
 import inkapplications.shade.lights.structures.AlertInfo
+import inkapplications.shade.lights.structures.DimmingInfo
 import inkapplications.shade.structures.PowerInfo
 import inkapplications.shade.structures.ResourceId
+import inkapplications.shade.structures.ResourceReference
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -17,10 +19,23 @@ data class GroupedLight(
     val id: ResourceId,
 
     /**
+     * Owner of the service
+     *
+     * In case the owner service is deleted, the service also gets deleted.
+     */
+    val owner: ResourceReference,
+
+    /**
      * Information about the power-state of the light group.
      */
     @SerialName("on")
     val powerInfo: PowerInfo? = null,
+
+    /**
+     * Information about the group's dimming, if supported.
+     */
+    @SerialName("dimming")
+    val dimmingInfo: DimmingInfo? = null,
 
     /**
      * Joined alert control for the light group.
