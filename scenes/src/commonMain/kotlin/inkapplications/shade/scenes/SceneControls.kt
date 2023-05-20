2f88072e06ebdb3b065a8ec6d6275e80b00baba3
Renee Vandervelde <Renee@ReneeVandervelde.com>
2023-05-20 07:45:34 -0500

    Add get-scene endpoint

--- scenes/src/commonMain/kotlin/inkapplications/shade/scenes/SceneControls.kt
+++ scenes/src/commonMain/kotlin/inkapplications/shade/scenes/SceneControls.kt
@@ -1,6 +1,7 @@
 package inkapplications.shade.scenes
 
 import inkapplications.shade.scenes.structures.Scene
+import inkapplications.shade.structures.ResourceId
 
 /**
  * Actions to get and configure Scenes on the Hue system.
@@ -12,4 +13,12 @@ interface SceneControls {
      * @return a list of all configured scenes on the Hue bridge.
      */
     suspend fun listScenes(): List<Scene>
+
+    /**
+     * Get information for a specific Scene.
+     *
+     * @param id The v2 resource ID of the Scene to get information for.
+     * @return information about the scene
+     */
+    suspend fun getScene(id: ResourceId): Scene
 }
--- scenes/src/commonMain/kotlin/inkapplications/shade/scenes/ShadeScenes.kt
+++ scenes/src/commonMain/kotlin/inkapplications/shade/scenes/ShadeScenes.kt
@@ -3,11 +3,16 @@ package inkapplications.shade.scenes
 import inkapplications.shade.internals.HueHttpClient
 import inkapplications.shade.internals.getData
 import inkapplications.shade.scenes.structures.Scene
+import inkapplications.shade.structures.ResourceId
 
 internal class ShadeScenes(
     private val hueClient: HueHttpClient,
 ): SceneControls {
     override suspend fun listScenes(): List<Scene> {
         return hueClient.getData("resource", "scene")
+    }
+
+    override suspend fun getScene(id: ResourceId): Scene {
+        return hueClient.getData<List<Scene>>("resource", "scene", id.value).single()
     }
 }
