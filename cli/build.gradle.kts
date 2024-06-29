f645dbd9b217d52199652be587cc2db7f10d9ab0
Renee Vandervelde <Renee@ReneeVandervelde.com>
2024-06-29 14:54:49 -0500

    Add SLF4J-NOP logger

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -11,5 +11,6 @@ application {
 dependencies {
     implementation(libs.coroutines.core)
     implementation("com.github.ajalt.clikt:clikt:4.4.0")
+    implementation("org.slf4j:slf4j-nop:1.7.36")
     implementation(projects.core)
 }
