820edf21d00eb52f39bfb81807b1c9eebe57c879
Renee Vandervelde <Renee@ReneeVandervelde.com>
2026-01-17 10:53:38 -0600

    Add get by ID endpoint for homekit module

--- homekit/src/commonMain/kotlin/inkapplications/shade/homekit/HomekitControls.kt
+++ homekit/src/commonMain/kotlin/inkapplications/shade/homekit/HomekitControls.kt
@@ -1,11 +1,19 @@
 package inkapplications.shade.homekit
 
 import inkapplications.shade.homekit.structures.Homekit
+import inkapplications.shade.structures.ResourceId
 
 /**
  * Actions to get homekit resources in the hue system.
  */
 interface HomekitControls {
+    /**
+     * Get the state of a single homekit resource.
+     *
+     * @param id The v2 resource ID of the homekit resource to fetch data for.
+     */
+    suspend fun getHomekit(id: ResourceId): Homekit
+
     /**
      * Get a list of homekit resources configured on the hue service.
      */
--- homekit/src/commonMain/kotlin/inkapplications/shade/homekit/ShadeHomekit.kt
+++ homekit/src/commonMain/kotlin/inkapplications/shade/homekit/ShadeHomekit.kt
@@ -3,6 +3,7 @@ package inkapplications.shade.homekit
 import inkapplications.shade.homekit.structures.Homekit
 import inkapplications.shade.internals.HueHttpClient
 import inkapplications.shade.internals.getData
+import inkapplications.shade.structures.ResourceId
 
 /**
  * Implements homekit controls via the hue client.
@@ -10,6 +11,10 @@ import inkapplications.shade.internals.getData
 internal class ShadeHomekit(
     private val hueHttpClient: HueHttpClient,
 ) : HomekitControls {
+    override suspend fun getHomekit(id: ResourceId): Homekit {
+        return hueHttpClient.getData<List<Homekit>>("resource", "homekit", id.value).single()
+    }
+
     override suspend fun listHomekit(): List<Homekit> {
         return hueHttpClient.getData("resource", "homekit")
     }
